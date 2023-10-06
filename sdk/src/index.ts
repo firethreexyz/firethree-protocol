5459957998cb033a192706cc5ffa0592eed6d806
dannpl <dancardosobr@gmail.com>
2023-10-06 01:00:21 -0300

    export files

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,6 +1,8 @@
-export * from './auth';
-export * from './analytics';
-export * from './collection';
-export * from './hosting'
-export * from './storage';
-export * from './project';
+import Auth from './auth'
+import Analytics from './analytics'
+import Collection from './collection'
+import Hosting from './hosting'
+import Storage from './storage'
+import Project from './project'
+
+export { Auth, Analytics, Collection, Hosting, Storage, Project }
