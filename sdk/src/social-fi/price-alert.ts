36c4c8a93043119b582420ead8dd5a9c1096f7d5
dannpl <dancardosobr@gmail.com>
2024-01-31 21:29:43 -0300

    add pyth price alerts

--- sdk/src/social-fi/price-alert.ts
+++ sdk/src/social-fi/price-alert.ts
@@ -1,14 +1,38 @@
 import { Program, Wallet } from '@coral-xyz/anchor'
 import { Connection } from '@solana/web3.js'
 import { Firethree } from '../types/firethree'
+import {
+  PythConnection,
+  getPythProgramKeyForCluster
+} from '@pythnetwork/client'
+import { PythVerbosePriceCallback } from '@pythnetwork/client/lib/PythConnection'
 
 export default class PriceAlert {
   program: Program<Firethree>
   connection: Connection
   wallet: Wallet
+  pythConnection: PythConnection
 
-  constructor(connection: Connection, wallet: Wallet) {
+  constructor(connection: Connection, wallet?: Wallet) {
     this.connection = connection
     this.wallet = wallet
+
+    this.pythConnection = new PythConnection(
+      connection,
+      getPythProgramKeyForCluster('mainnet-beta'),
+      'confirmed'
+    )
+  }
+
+  onPriceChange(callback: PythVerbosePriceCallback) {
+    this.pythConnection.onPriceChangeVerbose(callback)
+  }
+
+  async subscribe() {
+    await this.pythConnection.start()
+  }
+
+  async unsubscribe() {
+    await this.pythConnection.stop()
   }
 }
